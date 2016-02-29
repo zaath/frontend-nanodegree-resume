41c280e9525125214a795aafd5680c801977d581
Zaath <zebigmatt@hotmail.com>
2016-02-29 19:09:12 +0100

    Adding replace and append functions with awesome/fun thoughts

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,8 @@
-$("#main").append(["Matthieu Escaffre"]);
+// $("#main").append(["Matthieu Escaffre"]);
+var awesomeThoughts ="I'm Matthieu and I'm awesome!";
+var funThoughts = awesomeThoughts.replace("awesome", "fun");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
+
+$("#main").append(funThoughts);
